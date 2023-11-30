2cad5f29e95618b8e8c87818b1878060897b00ce
Rémi KORZENIOWSKI <r.korzeniowski@icloud.com>
2023-11-30 11:22:44 +0100

    style(navbar): basic style organization

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar.jsx';
 function App() {
   return (
     <div className="App">
-  
+      <Navbar/>
     </div>
   );
 }
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,3 +1,20 @@
+import '../styles/Navbar.scss';
+
 export default function Navbar() {
-    
+    return (
+        <div className="navbar-sct">
+            <div className="topbar-sct">
+                <div className="logo-sct"></div>
+                <div className="search-sct"></div>
+            </div>
+            <div className="botbar-sct">
+                <div className="allBtn-sct">
+                    <div className="btn-sct">
+                        <div id="img"></div>
+                        <div id="label"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
