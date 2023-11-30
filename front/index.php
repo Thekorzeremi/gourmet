7b686d70088f0a25f7c2451f76190482452868ae
Rémi KORZENIOWSKI <r.korzeniowski@icloud.com>
2023-11-30 13:59:10 +0100

    build(app): remove react app and creating vanilla files

--- front/index.php
+++ front/index.php
@@ -1,30 +1,26 @@
-import '../styles/Navbar.scss';
-import SearchIcon from '../assets/search-icon.svg';
-import BbkIcon from '../assets/bbk.png';
-import BoissonIcon from '../assets/boisson.png';
-import BurgerIcon from '../assets/burger.png';
-import DonutIcon from '../assets/donut.png';
-import GlaceIcon from '../assets/glace.png';
-import PizzaIcon from '../assets/pizza.png';
-import SaladeIcon from '../assets/salade.png';
-
-export default function Navbar() {
-    return (
-        <div className="navbar-sct">
-            <div className="topbar-sct">
-                <div className="logo-sct">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KOUIZINE</title>
+</head>
+<body>
+    <div class="navbar-sct">
+            <div class="topbar-sct">
+                <div class="logo-sct">
                     <p>KOUIZINE</p>
                 </div>
-                <div className="search-sct">
+                <div class="search-sct">
                     <input type="search" name="searchbar" id="searchbar" placeholder='Je cherche des recettes, des ingrédients...'/>
                     <div id="searchbtn">
                         <img src={SearchIcon}></img>
                     </div>
                 </div>
             </div>
-            <div className="botbar-sct">
-                <div className="allbtn-sct">
-                    <div className="btn-sct">
+            <div class="botbar-sct">
+                <div class="allbtn-sct">
+                    <div class="btn-sct">
                         <div id="img">
                             <img src={SaladeIcon} id='btnImg'></img>
                         </div>
@@ -32,7 +28,7 @@ export default function Navbar() {
                             <p>Salades</p>
                         </div>
                     </div>
-                    <div className="btn-sct">
+                    <div class="btn-sct">
                         <div id="img">
                             <img src={PizzaIcon} id='btnImg'></img>
                         </div>
@@ -40,7 +36,7 @@ export default function Navbar() {
                             <p>Pizzas</p>
                         </div>
                     </div>
-                    <div className="btn-sct">
+                    <div class="btn-sct">
                         <div id="img">
                             <img src={BurgerIcon} id='btnImg'></img>
                         </div>
@@ -48,7 +44,7 @@ export default function Navbar() {
                             <p>Burgers</p>
                         </div>
                     </div>
-                    <div className="btn-sct">
+                    <div class="btn-sct">
                         <div id="img">
                             <img src={BbkIcon} id='btnImg'></img>
                         </div>
@@ -56,7 +52,7 @@ export default function Navbar() {
                             <p>Barbecue</p>
                         </div>
                     </div>
-                    <div className="btn-sct">
+                    <div class="btn-sct">
                         <div id="img">
                             <img src={DonutIcon} id='btnImg'></img>
                         </div>
@@ -64,7 +60,7 @@ export default function Navbar() {
                             <p>Donuts</p>
                         </div>
                     </div>
-                    <div className="btn-sct">
+                    <div class="btn-sct">
                         <div id="img">
                             <img src={GlaceIcon} id='btnImg'></img>
                         </div>
@@ -72,7 +68,7 @@ export default function Navbar() {
                             <p>Glaces</p>
                         </div>
                     </div>
-                    <div className="btn-sct">
+                    <div class="btn-sct">
                         <div id="img">
                             <img src={BoissonIcon} id='btnImg'></img>
                         </div>
@@ -83,5 +79,15 @@ export default function Navbar() {
                 </div>
             </div>
         </div>
-    )
-}
+    <div class="content-sct">
+        <div class="trend-sct">
+            <div class='title-sct'>
+                <p>À la une de KOUIZINE</p>
+            </div>
+            <div class='grid-sct'>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- front/src/components/Content.jsx
+++
@@ -1,9 +0,0 @@
-import '../styles/Content.scss';
-
-export default function Content() {
-    return (
-        <div className="content-sct">
-            <div className="trend-sct"></div>
-        </div>
-    )
-}
